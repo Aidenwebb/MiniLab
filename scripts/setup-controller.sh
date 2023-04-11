2fbd896ed224af3395a70900547767acb59bbbbd
Aiden Arnkels-Webb <aiden@aidenwebb.com>
2023-04-11 15:50:12 +0100

    Add setup controller script

---
+++ scripts/setup-controller.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# This script is used to setup the controller node
+
+apt-get update
+apt-get upgrade
+
+# Install git, pip and make
+apt-get install -y git
+apt-get install -y python-pip
+apt-get install -y make
+
+# Install ansible
+
+pip install ansible
+
+# Install Docker
+
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker pi
