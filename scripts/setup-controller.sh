1842487768a8cfac09ed66da762da6b3a99c2258
Aiden Arnkels-Webb <aiden@aidenwebb.com>
2023-05-20 17:59:44 +0100

    Update controller script for Fedora/RHEL

--- scripts/setup-controller.sh
+++ scripts/setup-controller.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # This script is used to setup the controller node
 
-apt-get update
-apt-get upgrade
+sudo yum update -y
 
 # Install git, pip and make
-apt-get install -y git
-apt-get install -y python-pip
-apt-get install -y make
+sudo yum install -y git
+sudo yum install -y python-pip
+sudo yum install -y make
 
 # Install ansible
 
@@ -15,5 +14,5 @@ pip install ansible
 
 # Install Docker
 
-curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker pi
+sudo curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
